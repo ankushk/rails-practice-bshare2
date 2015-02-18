1d10b75f7d5a0e177b58d63de61263eb715fbcce
Ankush Kalkote <kalkote.ankush@gmail.com>
2015-02-18 10:54:18 +0530

    Fixes for ordering, deleting dependent records.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   
   def index
     if user_signed_in?
-      redirect_to :controller => 'dashboard', :action =>'index'
+      redirect_to dashboard_index_path
     end
   end
 
